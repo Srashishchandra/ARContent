cbdd8cc898c5de50e9108715670c3bf2929072a8
Ashish chandra <imashishchandra096@gmail.com>
2026-01-16 08:40:27 +0530

    Delete camera.js

--- camera.js
+++
@@ -1,8 +0,0 @@
-export async function startCamera(video) {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: { facingMode: "environment" }
-  });
-
-  video.srcObject = stream;
-  await video.play();
-}
